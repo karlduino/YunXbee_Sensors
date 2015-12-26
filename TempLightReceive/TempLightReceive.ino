283554270c1778f4149c68b7eeb7aebeea8b92e5
Karlduino <karlduino@gmail.com>
2015-12-26 13:13:34 -0600

    Revisions to TempLightReceive to work with Arduino Yun
     - Receiving "ExplicitRx" not just "Rx"
     - Deal with different pins for Xbee/Arduino connection on Yun
     - temp/light data rather than DHT22 temp/humidity data
     - Print to console rather than serial

--- TempLightReceive/TempLightReceive.ino
+++ TempLightReceive/TempLightReceive.ino
@@ -8,24 +8,35 @@
 // NO WARRANTY OF ANY KIND IS PROVIDED.
 //
 //
-// This example receives packets containing temperature and humidity
-// data, as sent by the DhtSend.ino sketch, and prints their contents to
-// serial.
+// This example receives packets containing temperature and light
+// data, as sent by the TempLightSend.ino sketch, and prints their contents.
+//
+// Modified to work with an Arduino Yun, printing the results to Console.
+// Using a Sparkfun Xbee shield, with pins 0-3 of Yun
+// not connected to the shield, and with shield pins 2 & 13 connected, and also
+// shield pins 3 and 5 connected (so that 13 is Rx, and 5 is Tx)
 
 #include <XBee.h>
 #include <Printers.h>
 #include <AltSoftSerial.h>
 #include "binary.h"
+#include <Console.h>
 
 XBeeWithCallbacks xbee;
 
 AltSoftSerial SoftSerial;
-#define DebugSerial Serial
+#define DebugSerial Console
 #define XBeeSerial SoftSerial
+
+#define ALTSS_USE_TIMER3
+#define INPUT_CAPTURE_PIN      13 // receive
+#define OUTPUT_COMPARE_A_PIN        5 // transmit
 
 void setup() {
   // Setup debug serial output
-  DebugSerial.begin(115200);
+  Bridge.begin();
+  Console.begin();
+  while(!Console); // wait for console to open
   DebugSerial.println(F("Starting..."));
 
   // Setup XBee serial communication
@@ -36,6 +47,7 @@ void setup() {
   // Setup callbacks
   xbee.onPacketError(printErrorCb, (uintptr_t)(Print*)&DebugSerial);
   xbee.onResponse(printErrorCb, (uintptr_t)(Print*)&DebugSerial);
+  xbee.onZBExplicitRxResponse(processRxPacket);
   xbee.onZBRxResponse(processRxPacket);
 }
 
@@ -44,12 +56,12 @@ void processRxPacket(ZBRxResponse& rx, uintptr_t) {
   uint8_t type = b.remove<uint8_t>();
 
   if (type == 1 && b.len() == 8) {
-    DebugSerial.print(F("DHT packet received from "));
+    DebugSerial.print(F("Temp/Light packet received from "));
     printHex(DebugSerial, rx.getRemoteAddress64());
     DebugSerial.println();
     DebugSerial.print(F("Temperature: "));
     DebugSerial.println(b.remove<float>());
-    DebugSerial.print(F("Humidity: "));
+    DebugSerial.print(F("Light: "));
     DebugSerial.println(b.remove<float>());
     return;
   }
@@ -57,6 +69,28 @@ void processRxPacket(ZBRxResponse& rx, uintptr_t) {
   DebugSerial.println(F("Unknown or invalid packet"));
   printResponse(rx, DebugSerial);
 }
+
+
+void processRxPacket(ZBExplicitRxResponse& rx, uintptr_t) {
+  Buffer b(rx.getData(), rx.getDataLength());
+  uint8_t type = b.remove<uint8_t>();
+
+  if (type == 1 && b.len() == 8) {
+    DebugSerial.print(F("Temp/Light (explicit) packet received from "));
+    printHex(DebugSerial, rx.getRemoteAddress64());
+    DebugSerial.println();
+    DebugSerial.print(F("Temperature: "));
+    DebugSerial.println(b.remove<float>());
+    DebugSerial.print(F("Light: "));
+    DebugSerial.println(b.remove<float>());
+    return;
+  }
+
+  DebugSerial.println(F("Unknown or invalid packet"));
+  printResponse(rx, DebugSerial);
+}
+
+
 
 void loop() {
   // Check the serial port to see if there is a new packet available
